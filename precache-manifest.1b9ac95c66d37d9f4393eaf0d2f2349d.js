e51f5659dd86ba092779f5bff7cc25371e05209a
andres cagua <andres.fcr089@gmail.com>
2021-12-11 16:37:53 -0500

    Updates

---
+++ precache-manifest.1b9ac95c66d37d9f4393eaf0d2f2349d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a396dc68c615b2dc82dbbd6c6c5d03",
+    "url": "/productos/index.html"
+  },
+  {
+    "revision": "2deb53cb2cc283fa08df",
+    "url": "/productos/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "afa0c6b0556d17c82115",
+    "url": "/productos/static/css/main.f965e2e4.chunk.css"
+  },
+  {
+    "revision": "2deb53cb2cc283fa08df",
+    "url": "/productos/static/js/2.4bb53fac.chunk.js"
+  },
+  {
+    "revision": "664f582eab2e489b2b1e397c056a20ac",
+    "url": "/productos/static/js/2.4bb53fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa0c6b0556d17c82115",
+    "url": "/productos/static/js/main.978ef1bc.chunk.js"
+  },
+  {
+    "revision": "89b56db11eb14530b8bf",
+    "url": "/productos/static/js/runtime-main.646f4c72.js"
+  }
+]);
